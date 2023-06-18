88c842f6bb859bb764b6561364420d53af4052a9
FabianMaEs <79736165+FabianMaEs@users.noreply.github.com>
2023-06-18 04:16:27 -0600

    Enviar correo cita

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const QRCode = require('qrcode');
-const nodemailer = require('nodemailer');
+const QRCode = require("qrcode");
+const nodemailer = require("nodemailer");
 
 app.use(cors());
 
@@ -21,7 +21,19 @@ app.get("/datos", (req, res) => {
     },
     {
       codigo: "XYZ789DEF456",
-      oferta: "Descuento del 30% en tu primer cita con el optometrista",
+      oferta: "Descuento del 20% en tu primer cita con el optometrista",
+    },
+    {
+      codigo: "123ABCXYZ456",
+      oferta: "Consulta gratuita con el dentista para niños menores de 6 años",
+    },
+    {
+      codigo: "456DEFXYZ789",
+      oferta: "Descuento del 15% en tu cita con el optometrista",
+    },
+    {
+      codigo: "789XYZABC123",
+      oferta: "Primer consulta gratuita con el veterinario",
     },
   ];
   res.json({ datos });
@@ -35,7 +47,19 @@ app.get("/codigoqr", (req, res) => {
     },
     {
       codigo: "XYZ789DEF456",
-      oferta: "Descuento del 30% en tu primer cita con el optometrista",
+      oferta: "Descuento del 20% en tu primer cita con el optometrista",
+    },
+    {
+      codigo: "123ABCXYZ456",
+      oferta: "Consulta gratuita con el dentista para niños menores de 6 años",
+    },
+    {
+      codigo: "456DEFXYZ789",
+      oferta: "Descuento del 15% en tu cita con el optometrista",
+    },
+    {
+      codigo: "789XYZABC123",
+      oferta: "Primer consulta gratuita con el veterinario",
     },
   ];
 
@@ -54,65 +78,96 @@ app.get("/codigoqr", (req, res) => {
 });
 
 // Ruta para el formulario de contacto
-app.post('/contacto', (req, res) => {
+app.post("/contacto", (req, res) => {
   // Obtener los datos
   const { name, apellido, email, message } = req.body;
 
-  console.log('Datos del formulario de contacto:');
-  console.log('Nombre: ', name);
-  console.log('Apellido', apellido);
-  console.log('Correo:', email);
-  console.log('Mensaje:', message);
+  console.log("Datos del formulario de contacto:");
+  console.log("Nombre: ", name);
+  console.log("Apellido", apellido);
+  console.log("Correo:", email);
+  console.log("Mensaje:", message);
 
   // Configurar el transporter de Nodemailer
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'miniproyectouno@gmail.com',
-      pass: 'skbhhywjlspgpxkz'
+      user: "miniproyectouno@gmail.com",
+      pass: "skbhhywjlspgpxkz",
+    },
+  });
+
+  // Configurar el mensaje de correo
+  const mailOptions = {
+    from: "miniproyectouno@gmail.com",
+    to: email,
+    subject: "¡Gracias por tu comentario, " + name + "!",
+    text: "Recibimos tu comentario y lo atenderemos lo antes posible.",
+  };
+
+  // Enviar correo
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error al enviar el correo:", error);
+    } else {
+      console.log("Correo enviado:", info.response);
     }
   });
+
+  res.json({ message: "Formulario de contacto enviado" });
+});
+
+app.post("/agendar", (req, res) => {
+  // Obtener los datos
+  const { formularioContacto, horario, medico } = req.body;
+
+  console.log("Datos del formulario de contacto:");
+  console.log("Nombre:", formularioContacto.nombre);
+  console.log("Apellido:", formularioContacto.apellido);
+  console.log("Correo:", formularioContacto.mail);
+  console.log("Tiempo:", formularioContacto.tiempo);
+  console.log("Horario:", horario);
+  console.log("Médico:", medico);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "miniproyectouno@gmail.com",
+      pass: "skbhhywjlspgpxkz",
+    },
+  });
+
+  const mailOptions = {
+    from: "miniproyectouno@gmail.com",
+    to: formularioContacto.mail,
+    subject: "Confirmación de cita médica",
+    text: `Hola ${formularioContacto.nombre} ${formularioContacto.apellido},
+    
+  Gracias por agendar una cita médica. Estos son los detalles de tu cita:
   
-  // Configurar el mensaje de correo para el primer correo
-const mailOptions1 = {
-  from: 'miniproyectouno@gmail.com',
-  to: email,
-  subject: '¡Gracias por tu comentario, ' + name + '!',
-  text: 'Recibimos tu comentario y lo atenderemos lo antes posible.'
-};  
+  Fecha y hora: ${horario} ${formularioContacto.tiempo}
+  Médico: ${medico}
+  
+  Si tienes alguna pregunta o necesitas realizar cambios en tu cita, no dudes en contactarnos.
+  
+  Saludos,
+  Equipo de Farmacias Parecidas, ¡parecido pero de un precio inferior!`,
+  };
 
-// Enviar el primer correo
-transporter.sendMail(mailOptions1, (error, info) => {
-  if (error) {
-    console.log('Error al enviar el correo:', error);
-  } else {
-    console.log('Correo enviado:', info.response);
-  }
-});
+  // Enviar correo
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error al enviar el correo:", error);
+    } else {
+      console.log("Correo enviado:", info.response);
+    }
+  });
 
-// Configurar el mensaje de correo para el segundo correo
-const mailOptions2 = {
-  from: 'miniproyectouno@gmail.com',
-  to: 'fabmac865@gmail.com',
-  subject: 'Comentario recibido de ' + name + ' ' + apellido,
-  text: 'Mensaje: ' + message
-};  
-
-// Enviar el segundo correo
-transporter.sendMail(mailOptions2, (error, info) => {
-  if (error) {
-    console.log('Error al enviar el correo:', error);
-  } else {
-    console.log('Correo enviado:', info.response);
-  }
-});
-
-
-  res.json({ message: 'Formulario de contacto enviado' });
+  res.json({ message: "Registro de cita enviado" });
 });
 
 // Inicia el servidor
-const port = 3000; // El puerto en el que deseas ejecutar tu servidor
+const port = 3000;
 app.listen(port, () => {
   console.log(`Servidor en el puerto ${port}`);
 });
